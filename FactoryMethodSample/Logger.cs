61dafb0d13c6004705e6e08419f3e92f0ec5fcb8
liupenghao <haochen214@163.com>
2019-06-23 23:34:13 +0800

    增加抽象产品角色

---
+++ FactoryMethodSample/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    /// <summary>
+    /// 日志记录器接口，充当抽象产品角色
+    /// </summary>
+    interface Logger
+    {
+        void WriteLog();
+    }
+}
