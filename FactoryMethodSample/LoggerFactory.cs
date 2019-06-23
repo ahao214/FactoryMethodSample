f888cd1226565eabb65a069edb59b0d673f815c4
liupenghao <haochen214@163.com>
2019-06-23 23:52:19 +0800

    添加抽象工厂角色

---
+++ FactoryMethodSample/LoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    /// <summary>
+    /// 日志记录器工厂接口,充当抽象工厂角色
+    /// </summary>
+    interface LoggerFactory
+    {
+        Logger CreateLogger();  //抽象工厂方法
+    }
+}
