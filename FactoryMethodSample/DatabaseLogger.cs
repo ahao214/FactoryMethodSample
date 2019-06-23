bba127c9a0fd7ba3fce092f758838e13a04f23bf
liupenghao <haochen214@163.com>
2019-06-23 23:48:00 +0800

    添加具体产品角色

---
+++ FactoryMethodSample/DatabaseLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    /// <summary>
+    /// 数据库日志记录器,充当具体产品角色
+    /// </summary>
+    class DatabaseLogger : Logger
+    {
+        public void WriteLog()
+        {
+            Console.WriteLine("数据库日志记录");
+        }
+    }
+}
---
+++ FactoryMethodSample/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    /// <summary>
+    /// 文件日志记录器,充当具体产品角色
+    /// </summary>
+    class FileLogger : Logger
+    {
+        public void WriteLog()
+        {
+            Console.WriteLine("文件日志记录");
+        }
+    }
+}
