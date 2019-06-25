aa941283dd00bb9d9d670b86216ea995747794c7
liupenghao <haochen214@163.com>
2019-06-25 21:22:27 +0800

    使用配置文件和反射

--- FactoryMethodSample/Program.cs
+++ FactoryMethodSample/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
 
 namespace FactoryMethodSample
 {
@@ -12,7 +14,13 @@ namespace FactoryMethodSample
         {
             LoggerFactory factory;
             Logger logger;
-            factory = new FileLoggerFactory();  //可引入配置文件实现
+            //factory = new FileLoggerFactory();  //可引入配置文件实现
+
+            // 读取配置文件
+            string factoryString = ConfigurationManager.AppSettings["factory"];
+            //反射生成对象
+            factory = (LoggerFactory)Assembly.Load("FactoryMethodSample").CreateInstance(factoryString);
+
             logger = factory.CreateLogger();
             logger.WriteLog();
 
